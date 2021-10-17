4a924355d7c7976c2c6e67fdd5418753e6fe09f4
llbar <ll.barsky08@gmail.com>
2021-10-17 16:28:20 -0500

    clock change

--- src/main.py
+++ src/main.py
@@ -74,7 +74,8 @@ def load_image(image, darken, colorKey = None):
 
 def main():
   
-    t = time.clock() #time of current frame
+    # t = time.clock() #time of current frame
+    t = time.perf_counter()
     oldTime = 0. #time of previous frame
     pygame.mixer.init()
     #pygame.mixer.music.load("MuseUprising.mp3")
@@ -150,7 +151,7 @@ def main():
         # timing for input and FPS counter
         
         frameTime = float(clock.get_time()) / 1000.0 # frameTime is the time this frame has taken, in seconds
-        t = time.clock()
+        t = time.perf_counter()
         #text = f.render(str(foot_time), False, (255, 255, 0))
         text = f.render(str(clock.get_fps()), False, (255, 255, 0))
         screen.blit(text, text.get_rect(), text.get_rect())
