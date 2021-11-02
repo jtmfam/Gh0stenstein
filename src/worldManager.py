a22088ed661d16f7b89c29e0a67ef8a0717a6b2d
Ari Shater <ashater@gmail.com>
2021-11-02 17:22:06 -0400

    weapon firing AND Leo's changes

--- src/worldManager.py
+++ src/worldManager.py
@@ -13,7 +13,7 @@ class WorldManager(object):
               load_image(pygame.image.load("pics/items/greenlight.png").convert(), False, colorKey = (0,0,0)),
               load_image(pygame.image.load("pics/items/pinky_l.png").convert(), False, colorKey = (0,0,0)),
               load_image(pygame.image.load("pics/items/pinky_r.png").convert(), False, colorKey = (0,0,0)),
-              load_image(pygame.image.load("pics/items/bullet_gen.png").convert(), False, colorKey=(0, 0, 0)),
+              load_image(pygame.image.load("pics/items/bullet_gen.bmp").convert(), False, colorKey=(0, 0, 0)),
 
         ]
         
